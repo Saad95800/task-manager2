4b8eb35a3c1d8204715e59322e1e012caddbd7b4
Saad RAJRAJI <rsmailpro@gmail.com>
2023-12-22 14:07:27 +0100

    Corection des commentaires DragnDrop

--- src/component/Tables.jsx
+++ src/component/Tables.jsx
@@ -74,22 +74,24 @@ export default function Tables() {
 
             if(Number(order_table_drop > Number(order_table_drag))){
                 // le tableau qui a l'id id_table_drag prend le order order_table_drop
-                // Les tableaux d'order inférieur à order_table_drop et supérieur à order_table_drag on leur order qui fait -1
+                // Les tableaux d'order inférieur à order_table_drop et supérieur à order_table_drag on leur order qui se décrémente de 1
                 if(table.id.toString() === id_table_drag.toString()){
                     table.order = Number(order_table_drop)
                 }else if(table.id.toString() === id_table_drop.toString()){
                     table.order = table.order - 1
+                    // On décale à gauche les tableaux situés à gauche du tableau de drop
                 }else if(Number(table.order) < Number(order_table_drop) && Number(table.order) > Number(order_table_drag)){
                     table.order = table.order - 1
                 }
                 // Si l'order du tableau de drop est inférieur à l'order du tableau de drag          
             }else if(Number(order_table_drop < Number(order_table_drag))){
                 // le tableau qui a l'id id_table_drag prend le order order_table_drop
-                // Les tableaux d'order suppérieur à order_table_drop et inférieur à order_table_drag on leur order qui fait -1
+                // Les tableaux d'order suppérieur à order_table_drop et inférieur à order_table_drag on leur order qui s'incrémente de 1
                 if(table.id.toString() === id_table_drag.toString()){
                     table.order = Number(order_table_drop)
                 }else if(table.id.toString() === id_table_drop.toString()){
                     table.order = table.order + 1
+                    // On décale à droite les tableaux situés à droite du tableau de drop
                 }else if(Number(table.order) > Number(order_table_drop) && Number(table.order) < Number(order_table_drag)){
                     table.order = table.order + 1
                 }  
